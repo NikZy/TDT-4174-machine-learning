5761ec6e33a4360f8fc15a80a478c8e89103a1fd
Sindre Sivertsen <sindrejohan1@gmail.com>
2020-11-26 12:56:17 +0100

    Requests from frontend works

--- source/webapp/housing-price-estimator/src/App.tsx
+++ source/webapp/housing-price-estimator/src/App.tsx
@@ -4,12 +4,35 @@ import './App.css';
 import { isVariableDeclaration } from 'typescript';
 import { features as Features } from 'process';
 import { maxHeaderSize } from 'http';
+import { useForm } from 'react-hook-form';
 
+const ApiPost = (url, data) =>
+  fetch(`http://localhost:5000/` + url, {
+    method: 'POST',
+    mode: 'cors',
+    cache: 'no-cache',
+    credentials: 'same-origin',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data), // body data type must match "Content-Type" header
+  }).then((res) => res.json());
 function App() {
+  const { register, handleSubmit, watch, errors } = useForm();
   const predictedPriceTest = estimate_price(normalizeParameters(test));
+  const onSubmit = (data) => {
+    console.log(data);
+    const dataConvertedToInts = convertIntObj(data);
+    console.log(convertIntObj(data));
+    const features = Object.assign(test, dataConvertedToInts);
+    console.log(features);
+    ApiPost('estimator', features)
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+  };
   console.log(test);
-  console.log(normalizeParameters(test));
-  console.log(estimate_price(normalizeParameters(test)));
+  // console.log(normalizeParameters(test));
+  // console.log(estimate_price(normalizeParameters(test)));
   return (
     <div className="App">
       <header className="App-header">
@@ -26,6 +49,16 @@ function App() {
           Learn React
         </a>
       </header>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          type="number"
+          name="bedrooms"
+          defaultValue="2"
+          ref={register({ required: true })}
+        />
+        {errors.bedroom && <span>This field is required</span>}
+        <input type="submit" />
+      </form>
       <h1>
         {predictedPriceTest ? estimate_price(normalizeParameters(test)) : 0}
       </h1>
@@ -44,6 +77,16 @@ const calculateLastFixed = (built: number, renovate: number) => {
                                    df.at[i, 'yr_renovated'])) / df.at[i, 'sqft_living'] */
   return 2020 - Math.max(built, renovate);
 };
+
+function convertIntObj(obj) {
+  const res = {};
+  for (const key in obj) {
+    const parsed = parseInt(obj[key]);
+    res[key] = parsed;
+    res[key] = isNaN(parsed) ? obj[key] : parsed;
+  }
+  return res;
+}
 interface Features {
   bathrooms: number;
   bedrooms: number;
@@ -81,6 +124,18 @@ const test: Features = {
   sqft_lot15: 5650,
   center_distance: calculateCenterDistance(47.5112, -122.257),
 };
+/* const test2: Features = {
+  bedrooms: 2,
+  bathrooms: -0.3987371485515761,
+  sqft_living:-1.4474635685648216,
+  sqft_lot: -0.9036171393191288,
+  floors: -0.1287548306162246,
+  waterfront: -0.9154270041565511,
+  view: -0.08717263102067989,
+  condition: -0.30575946383494007,
+  grade: 0.9075535361389149,
+  sqft_above: -0.5588357490736213,
+  -0.6501739072174175,-0.6586810403679764,-1.400644831402753,-1.0308984565214254,-0.14351452261811226,-1.3417547735375033,0.41231859187110054 */
 const normalizeParameters = (variables: Features): Features => {
   // subracting the average
   // dividing by standard deviation
@@ -99,28 +154,29 @@ const normalizeParameters = (variables: Features): Features => {
     zip_code: (variables.zip_code - 98077.94) / 53.51,
     sqft_living15: (variables.sqft_living15 - 1986.55) / 685.4,
     sqft_lot15: (variables.sqft_lot15 - 12768.45) / 27304.18,
-    center_distance: (variables.center_distance - -0.18773) / 0.094203,
+    center_distance: (variables.center_distance - 0.18773) / 0.094203,
     last_fixed: (variables.last_fixed - 0.029588) / 0.027193,
   };
 };
 const estimate_price = (variables: Features): number => {
   return (
-    -1.20644943e4 * variables.bedrooms +
-    1.68605256e4 * variables.bathrooms + // bathrooms
-    6.00033306e4 * variables.sqft_living + // sqft_living
-    2.28164262e4 * variables.sqft_lot + // sqft_lot
-    1.96835599e4 * variables.floors + // floors
-    1.31876732e-11 * variables.waterfront + //waterfont
-    3.71400533e4 * variables.view + // view
-    1.99840531e4 * variables.condition + //  condition
-    9.86876017e4 * variables.grade + // grade
-    4.84840553e4 * variables.sqft_above + // sqft_above
-    3.38028639e4 * variables.sqft_basement + // sqft_basement code
-    4.50330596e3 * variables.zip_code + //zip
-    1.79925267e4 * variables.sqft_living15 + //  sqft_living15
-    1.80663354e4 * variables.sqft_lot15 + // sqft_sqft_lot15
-    9.19778574e4 * variables.center_distance + // center_distance
-    6.00239138e4 * variables.last_fixed //  last_fixed
+    -12064.4943 * variables.bedrooms +
+    16860.5256 * variables.bathrooms + // bathrooms
+    60003.3306 * variables.sqft_living + // sqft_living
+    22816.4262 * variables.sqft_lot + // sqft_lot
+    19683.5599 * variables.floors + // floors
+    53372.09013601506 * variables.waterfront + //waterfont
+    37140.0533 * variables.view + // view
+    19984.0531 * variables.condition + //  condition
+    98687.6017 * variables.grade + // grade
+    48484.0553 * variables.sqft_above + // sqft_above
+    33802.8639 * variables.sqft_basement + // sqft_basement code
+    4503.30596 * variables.zip_code + //zip
+    17992.5267 * variables.sqft_living15 + //  sqft_living15
+    -18066.3354 * variables.sqft_lot15 + // sqft_sqft_lot15
+    91977.8574 * variables.center_distance + // center_distance
+    60023.9138 * variables.last_fixed + //  last_fixed
+    524327 * 1
   );
 };
 
